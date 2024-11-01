fb170d31702510744cad8c55a3f7d2f044a00da2
Mateus Roni <mateus-roni@hotmail.com>
2024-11-01 15:13:00 -0300

    :wrench: update: add round trip && fix data collection for new google flights page

--- src/flights/index.ts
+++ src/flights/index.ts
@@ -1,6 +1,12 @@
-import webdriver, { Builder, By, Key, until } from 'selenium-webdriver';
+import webdriver, {
+  Builder,
+  By,
+  Key,
+  until,
+  WebDriver
+} from 'selenium-webdriver';
 import 'chromedriver';
-import Chrome from 'selenium-webdriver/chrome';
+import Chrome, { ServiceBuilder } from 'selenium-webdriver/chrome';
 import sleep from 'utils/sleep';
 import soundAlarm from 'utils/soundAlarm';
 
@@ -13,13 +19,14 @@ const inputInitialInfo = async (driver: webdriver.WebDriver) => {
 
   await option.click();
 
-  option = await driver.findElement(
-    By.xpath(
-      '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[2]/ul/li[2]'
-    )
-  );
+  // option = await driver.findElement(
+  //   By.xpath(
+  //     '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[1]/div[1]/div/div/div/div[2]/ul/li[2]'
+  //   )
+  // );
 
-  await option.click();
+  // await option.click();
+
   await sleep(1000);
   // escolhe destino
   await driver.actions().sendKeys(Key.TAB, Key.TAB, Key.TAB).perform();
@@ -48,31 +55,40 @@ const inputInitialInfo = async (driver: webdriver.WebDriver) => {
   await sleep(1000);
   await driver
     .actions()
-    .sendKeys(process.env.INITIAL_DATE || '05/08/2023', Key.TAB, Key.ENTER)
+    .sendKeys(process.env.INITIAL_DATE || '05/08/2023')
     .perform();
+
+  await sleep(1000);
+  await driver
+    .actions()
+    .sendKeys(Key.TAB, process.env.FINISH_DATE || '')
+    .perform();
+
+  await sleep(1000);
+  await driver.actions().sendKeys(Key.TAB, Key.TAB, Key.ENTER).perform();
 };
 
 const searchForGoodFlights = async (
   driver: webdriver.WebDriver,
-  date: number
+  initial_date: number
 ) => {
   const lis = await driver.findElements(By.className('pIav2d'));
 
   for (const li of lis) {
     const duration = await (
-      await li.findElement(By.xpath('.//div/div[2]/div/div[2]/div[3]/div'))
+      await li.findElement(By.xpath('.//div/div[2]/div/div[2]/div/div[3]/div'))
     ).getText();
 
     const price = await (
       await li.findElement(
-        By.xpath('.//div/div[2]/div/div[2]/div[6]/div/div[2]/span')
+        By.xpath('.//div/div[2]/div/div[2]/div/div[6]/div[1]/div[2]/span')
       )
     ).getText();
 
     const leaveTime = await (
       await li.findElement(
         By.xpath(
-          './/div/div[2]/div/div[2]/div[2]/div[1]/span/span[1]/span/span/span'
+          './/div/div[2]/div/div[2]/div/div[2]/div[1]/span/span[1]/span/span/span'
         )
       )
     ).getText();
@@ -80,21 +96,22 @@ const searchForGoodFlights = async (
     const arrivalTime = await (
       await li.findElement(
         By.xpath(
-          './/div/div[2]/div/div[2]/div[2]/div[1]/span/span[2]/span/span/span'
+          './/div/div[2]/div/div[2]/div/div[2]/div[1]/span/span[2]/span/span/span'
         )
       )
     ).getText();
 
     const numberOfHours = Number(duration.split(' ')[0]);
-    const formattedPrice = Number(price.split(' ')[1].replace('.', ''));
+    const formattedPrice =
+      Number(price.split(' ')[1]?.replace?.('.', '')) || 99999;
     const month = process.env.INITIAL_DATE?.split('/')[1];
 
     if (
-      numberOfHours < Number(process.env.MAX_FLIGHT_TIME) &&
+      numberOfHours < Number(process.env.MAX_FLIGHT_TIME || 999) &&
       formattedPrice < Number(process.env.MAX_PRICE)
     ) {
       console.log(
-        `${date}/${month} ${leaveTime} - ${arrivalTime} | ${numberOfHours}h de voo | R$${formattedPrice}`
+        `${initial_date}/${month} ${leaveTime} - ${arrivalTime} | ${numberOfHours}h de voo | R$${formattedPrice}`
       );
       await soundAlarm();
     }
@@ -102,39 +119,51 @@ const searchForGoodFlights = async (
 };
 
 const flights = async () => {
+  // options.addArguments('--disable-extensions');
+  // options.addArguments('--disable-gpu');
+
   const options = new Chrome.Options();
   options.windowSize({
     width: 1440,
     height: 1300
   });
-  // options.addArguments('--disable-extensions');
-  // options.addArguments('--headless');
-  // options.addArguments('--disable-gpu');
+
   // options.addArguments('--no-sandbox');
+  // options.addArguments('--disable-dev-shm-usage');
+  // options.addArguments('--headless');
+
+  const service = new ServiceBuilder();
 
   const driver = await new Builder()
     .forBrowser('chrome')
     .setChromeOptions(options)
     .build();
+
   await driver.get('https://www.google.com/travel/flights?hl=pt-BR');
 
-  await sleep(10000);
+  await sleep(5000);
 
   try {
     await inputInitialInfo(driver);
     await sleep(10000);
-    const rightArrow = await driver.findElement(
+    const rightArrowInitialDate = await driver.findElement(
       By.xpath(
+        // '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/div[3]/button'
         '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/div[3]/button'
       )
     );
+    const rightArrowFinishDate = await driver.findElement(
+      By.xpath(
+        '/html/body/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[2]/div/div[2]/button'
+      )
+    );
 
-    let date = Number(process.env.INITAL_DATE?.split('/')[0]);
+    let date = Number(process.env.INITIAL_DATE?.split('/')[0]);
     await searchForGoodFlights(driver, Number(date));
 
     for (let i = 1; i <= Number(process.env.DAYS_ON_WINDOW); i++) {
-      await rightArrow.click();
-      await sleep(10000);
+      await rightArrowFinishDate.click();
+      await sleep(5000);
       await searchForGoodFlights(driver, date + i);
     }
   } catch (error) {
